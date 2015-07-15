722a4011895d878a44282749bc54a5c243dc3134
bajdcc <bajdcc@foxmail.com>
2015-07-15 16:12:39 +0800

    v4.7.1

---
+++ SimuCircult/Common/Drawing/DrawHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace System.Drawing
+{
+	public static class DrawHelper
+	{
+		public static Rectangle AdjustBound(this Rectangle absBound, Rectangle relBound)
+		{
+			var ext = absBound;
+			ext.Offset(relBound.Location);
+			ext.Intersect(absBound);
+			return ext;
+		}
+	}
+}
